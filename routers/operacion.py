47dbbbc394f34691a2860c49f1e07011d093ad7e
Gabriel Ramos <109390599+gabrielramos02@users.noreply.github.com>
2024-02-19 14:45:48 -0500

    fixing branch

--- routers/operacion.py
+++ routers/operacion.py
@@ -33,7 +33,7 @@ async def add_operacion(
         Solicitud_Operacion,
         Solicitud_Operacion.fecha_solicitud == operacion.fecha_solicitud,
     )
-
+    
     if operacion_db != None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="La operacion ya existe"
