3fcc088da97f7642d6a93fb2762d095f106ceb67
Gabriel Ramos <109390599+gabrielramos02@users.noreply.github.com>
2024-03-06 19:48:24 +0000

    fixed )

--- db/client.py
+++ db/client.py
@@ -3,5 +3,5 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 client = AsyncIOMotorClient(os.environ.get("BD_STRING")
 
-db_client = AIOEngine(client=client,database="test")
+db_client = AIOEngine(client=client,database="test"))
 
