cd68e61ab98a239e85040f2f316902129f457294
Gabriel Ramos <109390599+gabrielramos02@users.noreply.github.com>
2024-03-10 13:05:29 -0400

    added cama vacia

--- db/schemas/initial_data.py
+++ db/schemas/initial_data.py
@@ -1,5 +1,5 @@
 from db.client import db_client
-from db.models.imports import Cama,Sala,User
+from db.models.imports import Cama, Sala, User
 from passlib.context import CryptContext
 
 crypth = CryptContext(schemes=["bcrypt"])
@@ -9,24 +9,27 @@ async def valores_iniciales():
     salas_existe = await db_client.find(Sala)
     director_existe = await db_client.find(User)
     if director_existe.__len__ == 0:
-        director = User(username="director",password=crypth.encrypt("director"),role="director")
+        director = User(
+            username="director", password=crypth.encrypt("director"), role="director"
+        )
         await db_client.save(director)
-    if salas_existe.__len__() != 0 :
+    if salas_existe.__len__() != 0:
         return
     salas = []
     camas = []
-    for numero_sala in range (1,21):
+    for numero_sala in range(1, 21):
         sala = Sala(numero=str(numero_sala))
         salas.append(sala)
         camas = []
-        for cama_actual in range(1,11):
-            cama = Cama(numero=str(cama_actual),paciente=None,sala=sala)
+        for cama_actual in range(1, 11):
+            cama = Cama(numero=str(cama_actual), paciente=None, sala=sala)
             camas.append(cama)
             await db_client.save_all(camas)
+    sala_vacia = Sala(numero="vacia")
+
+    cama_vacia = Cama(numero="vacia", paciente=None, sala=sala_vacia, ocupada=True)
+    await db_client.save(cama_vacia)
+    salas.append(sala_vacia)
     await db_client.save_all(salas)
-    
+
     return
-
-
-
-
